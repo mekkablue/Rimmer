645c5dc14159fddc0c1c6a9fe7ab3204db0e78d2
Rainer Erich Scheichelbauer <res@glyphsapp.com>
2018-02-06 17:23:00 +0100

    Initial Release

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # Rimmer
-A Glyphs.app filter for creating rims around your glyphs
+
+This is a plugin for the [Glyphs font editor](http://glyphsapp.com/) by Georg Seifert.
+It adds rims around your glyphs. After installation, it will add the menu item *Filter > Rimmer*.
+You can set a keyboard shortcut in System Preferences.
+
+![Rimmer in action.](Rimmer.png "Font: Hyperlipse by Schriftlabor and mucca design.")
+
+
+### Installation
+
+1. Go to *Window > Plugin Manager.*
+2. Click on the *Install* button next to *Rimmer.*
+3. Restart the app.
+
+### Usage Instructions
+
+1. Select a glyph in Edit view, or select any number of glyphs in Font or Edit View.
+2. Choose *Filter > Rimmer*.
+
+Alternatively, you can also use it as a custom parameter. Click on the gear menu in the lower left to copy such a parameter into the clipboard. Paste it into the custom parameters of an instance.
+
+At the end of the parameter value, you can hang `exclude:` or `include:`, followed by a comma-separated list of glyph names. This will apply the filter only to the included glyphs, or the glyphs not excluded, respectively.
+
+### Requirements
+
+The plugin has only been tested for Glyphs 2.5.x on macOS Sierra. It may not work on older configurations.
+
+### License
+
+Copyright 2018 Rainer Erich Scheichelbauer (@mekkablue).
+Based on sample code by Georg Seifert (@schriftgestalt) and Jan Gerner (@yanone).
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+See the License file included in this repository for further details.
---
+++ Rimmer.glyphsFilter/Contents/Resources/plugin.py
@@ -0,0 +1,116 @@
+# encoding: utf-8
+
+###########################################################################################################
+#
+#
+#	Filter with dialog Plugin
+#
+#	Read the docs:
+#	https://github.com/schriftgestalt/GlyphsSDK/tree/master/Python%20Templates/Filter%20with%20Dialog
+#
+#	For help on the use of Interface Builder:
+#	https://github.com/schriftgestalt/GlyphsSDK/tree/master/Python%20Templates
+#
+#
+###########################################################################################################
+
+import objc
+from GlyphsApp import *
+from GlyphsApp.plugins import *
+
+class Rimmer(FilterWithDialog):
+	
+	# Definitions of IBOutlets
+	dialog = objc.IBOutlet()
+	paddingField = objc.IBOutlet()
+	rimField = objc.IBOutlet()
+	
+	def settings(self):
+		self.menuName = "Rimmer"
+		
+		# Word on Run Button (default: Apply)
+		self.actionButtonLabel = Glyphs.localize({'en': u'Rim', 'de': u'Erweitern'})
+		
+		# Load dialog from .nib (without .extension)
+		self.loadNib('IBdialog', __file__)
+	
+	# On dialog show
+	def start(self):
+		
+		# Set default value
+		Glyphs.registerDefault('com.mekkablue.Rimmer.padding', 15.0)
+		Glyphs.registerDefault('com.mekkablue.Rimmer.rim', 10.0)
+		
+		# Set value of text field
+		self.paddingField.setStringValue_(Glyphs.defaults['com.mekkablue.Rimmer.padding'])
+		self.rimField.setStringValue_(Glyphs.defaults['com.mekkablue.Rimmer.rim'])
+		
+		# Set focus to text field
+		self.paddingField.becomeFirstResponder()
+		
+	# Actions triggered by UI
+	@objc.IBAction
+	def setPadding_( self, sender ):
+		Glyphs.defaults['com.mekkablue.Rimmer.padding'] = sender.floatValue()
+		self.update()
+	
+	@objc.IBAction
+	def setRim_( self, sender ):
+		Glyphs.defaults['com.mekkablue.Rimmer.rim'] = sender.floatValue()
+		self.update()
+	
+	# Actual filter
+	def filter(self, layer, inEditView, customParameters):
+		# Set defaults
+		padding = 15.0
+		rim = 10.0
+		
+		if not inEditView:
+			# Called on font export, get value from customParameters
+			if customParameters.has_key('padding'):
+				padding = float(customParameters['padding'])
+			if customParameters.has_key('rim'):
+				rim = float(customParameters['rim'])
+		else:
+			# Called through UI, use stored value
+			padding = float(Glyphs.defaults['com.mekkablue.Rimmer.padding'])
+			rim = float(Glyphs.defaults['com.mekkablue.Rimmer.rim'])
+		
+		# prepare inner part:
+		coreLayer = layer.copyDecomposedLayer()
+		coreLayer.removeOverlap()
+		coreLayer.addInflectionPoints()
+		coreLayer.correctPathDirection()
+		
+		# prepare rim:
+		rimLayer = coreLayer.copy()
+		self.offsetLayer( rimLayer, padding, makeStroke=False, position=0.0, autoStroke=False )
+		self.offsetLayer( rimLayer, rim, makeStroke=True, position=0.0, autoStroke=False )
+		
+		# clean out original layer, and add core+rim:
+		layer.components = None
+		layer.hints = None
+		layer.paths = coreLayer.paths
+		layer.paths.extend( rimLayer.paths )
+		
+	
+	def offsetLayer( self, thisLayer, offset, makeStroke=False, position=0.5, autoStroke=False ):
+		offsetFilter = NSClassFromString("GlyphsFilterOffsetCurve")
+		offsetFilter.offsetLayer_offsetX_offsetY_makeStroke_autoStroke_position_error_shadow_(
+			thisLayer,
+			offset, offset, # horizontal and vertical offset
+			makeStroke,     # if True, creates a stroke
+			autoStroke,     # if True, distorts resulting shape to vertical metrics
+			position,       # stroke distribution to the left and right, 0.5 = middle
+			None, None )
+	
+	def generateCustomParameter( self ):
+		return "%s; padding:%s; rim:%s" % (
+			self.__class__.__name__, 
+			Glyphs.defaults['com.mekkablue.Rimmer.padding'],
+			Glyphs.defaults['com.mekkablue.Rimmer.rim'],
+			)
+	
+	def __file__(self):
+		"""Please leave this method unchanged"""
+		return __file__
